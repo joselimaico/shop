8c41867dff86b9dc479d779df01bcf67f518190d
limaico <francisco.limaico@kushkipagos.com>
2024-04-07 19:38:32 -0500

    add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <h1>hola mundo</h1>
+      <h1 className={titleFont.className}>hola mundo</h1>
     </main>
   );
 }
