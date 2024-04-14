00ee3bc08139019201f9d475d30b06f857fa23b9
limaico <francisco.limaico@kushkipagos.com>
2024-04-14 17:06:14 -0500

    add categories and  half product detail

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,36 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
-  params :{
-    id: string;
-  }
+  params: {
+    id: Category;
+  };
 }
+const products = initialData.products;
 
-export default function ({params}:Props) {
-
-  const {id} = params;
-
-  if(id ==="kids"){
-    notFound();
-  }
+export default function ({ params }: Props) {
+  const { id } = params;
+  const filteredProducts = products.filter((product) => product.gender === id);
+  // if(id ==="kids"){
+  //   notFound();
+  // }
+  const names: Record<Category, string> = {
+    men: "para hombres",
+    women: "para mujeres",
+    kid: "para niño",
+    unisex: "para Todos"
+  };
 
   return (
-    <div className="">
-      <h1>category Page {id} </h1>
-    </div>
+    <>
+      <Title
+        title={`Productos ${names[id]}`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+      <ProductGrid products={filteredProducts} />
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/title/Title";
-export * from "./ui/not-found/PageNotFound";
 export * from "./ui/sidebar/Sidebar";
+export * from "./ui/not-found/PageNotFound";
 
-
-export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
+export * from "./products/product-grid/ProductGrid";
+
+export * from "./product/size-selector/SizeSelector";
+export * from "./product/quantity-selector/QuantitySelector";
