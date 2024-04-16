b14834dd4c547d6a86de8c042d9a36801a1efb59
limaico <francisco.limaico@kushkipagos.com>
2024-04-16 16:49:19 -0500

    add slideshow desktop and mobile

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,13 @@
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
 
-import { initialData } from '@/seed/seed';
-import { titleFont } from '@/config/fonts';
-import { QuantitySelector, SizeSelector } from '@/components';
+import { initialData } from "@/seed/seed";
+import { titleFont } from "@/config/fonts";
+import {
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  QuantitySelector,
+  SizeSelector,
+} from "@/components";
 
 interface Props {
   params: {
@@ -10,76 +15,56 @@ interface Props {
   };
 }
 
-
-
-export default function ( { params }: Props ) {
-
+export default function ({ params }: Props) {
   const { slug } = params;
-  const product = initialData.products.find( product => product.slug === slug );
+  const product = initialData.products.find((product) => product.slug === slug);
 
-  if ( !product ) {
+  if (!product) {
     notFound();
   }
 
-
-
   return (
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
-
-      {/* Slideshow */ }
+      {/* Slideshow */}
       <div className="col-span-1 md:col-span-2 ">
-        
         {/* Mobile Slideshow */}
-        {/* <ProductMobileSlideshow 
-          title={ product.title }
-          images={ product.images }
+        <ProductMobileSlideshow
+          title={product.title}
+          images={product.images}
           className="block md:hidden"
-        /> */}
-        
+        />
+
         {/* Desktop Slideshow */}
-        {/* <ProductSlideshow 
-          title={ product.title }
-          images={ product.images }
+        <ProductSlideshow
+          title={product.title}
+          images={product.images}
           className="hidden md:block"
-        /> */}
-
-        
+        />
       </div>
 
-      {/* Detalles */ }
+      {/* Detalles */}
       <div className="col-span-1 px-5">
-
-        <h1 className={ ` ${ titleFont.className } antialiased font-bold text-xl` }>
-          { product.title }
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
         </h1>
-        <p className="text-lg mb-5">${ product.price }</p>
+        <p className="text-lg mb-5">${product.price}</p>
 
-        {/* Selector de Tallas */ }
+        {/* Selector de Tallas */}
         <SizeSelector
-          selectedSize={ product.sizes[ 1 ] }
-          availableSizes={ product.sizes }
+          selectedSize={product.sizes[1]}
+          availableSizes={product.sizes}
         />
 
+        {/* Selector de Cantidad */}
+        <QuantitySelector quantity={2} />
 
-        {/* Selector de Cantidad */ }
-        <QuantitySelector 
-          quantity={ 2 }
-        />
+        {/* Button */}
+        <button className="btn-primary my-5">Agregar al carrito</button>
 
-
-        {/* Button */ }
-        <button className="btn-primary my-5">
-          Agregar al carrito
-        </button>
-
-        {/* Descripción */ }
+        {/* Descripción */}
         <h3 className="font-bold text-sm">Descripción</h3>
-        <p className="font-light">
-          { product.description }
-        </p>
-
+        <p className="font-light">{product.description}</p>
       </div>
-
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,5 +6,9 @@ export * from "./ui/not-found/PageNotFound";
 export * from "./products/product-grid/ProductGridItem";
 export * from "./products/product-grid/ProductGrid";
 
+export * from "./product/slideshow/ProductSlideshow";
+export * from "./product/slideshow/ProductMobileSlideshow";
 export * from "./product/size-selector/SizeSelector";
 export * from "./product/quantity-selector/QuantitySelector";
+
+
