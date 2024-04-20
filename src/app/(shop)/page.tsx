03d364e514a8039e37b75796452eab22e8fb80b9
limaico <francisco.limaico@kushkipagos.com>
2024-04-20 16:23:19 -0500

    add query for pagination

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,19 +1,24 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const products = initialData.products;
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
 
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products } = await getPaginatedProductsWithImages({ page });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
   return (
     <>
-     <Title
-        title="Tienda"
-        subtitle="Todos los productos"
-        className="mb-2"
-      /> 
-      <ProductGrid 
-        products={ products }
-      />
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+      <ProductGrid products={products} />
     </>
   );
 }
